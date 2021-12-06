1ec83f6d47cef6563788b122dc9eb62214497912
Alvin Gomez <alvingomez.world@gmail.com>
2021-12-06 14:27:13 +0800

    Route pages
    Setup route and pages to be routed.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import CurrentWork from './pages/CurrentWork';
+import Resume from "./pages/Resume";
+import Contact from './pages/Contact';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/currentwork" component={CurrentWork} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/CurrentWork.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CurrentWork() {
+    return (
+        <div>
+            <h1>Current Work</h1>
+        </div>
+    )
+}
+
+export default CurrentWork
