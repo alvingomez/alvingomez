62df383a85b649a4e55f68c58068de207f137d5c
Alvin Gomez <alvingomez.world@gmail.com>
2021-12-21 22:55:28 +0800

    Header component
    Pass data as props to the header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,29 @@
 import { useState } from "react";
 
-function Header() {
+function Header({ heading1, heading2, heading3, children }) {
   const [state] = useState({
     image: {
       monster3: "/assets/images/monster-3.png",
     },
   });
 
-  return (    
-      <>      
-        <div className="header__content">
-          <h3>Web Developoment And Style</h3>
-          <h1>Alvin Gomez</h1>
-          <h2>Front-end developer</h2>   
-          <button className="btn">Check me out </button>       
-        </div>
-        
-        <div className="header__image header__image--background-color">
-          <img
-            src={state.image.monster3}
-            alt="monster blue and pink"
-            className="header__image--monster3"
-          ></img>
-        </div>
-      </>
-     
-    
-  
+  return (
+    <>
+      <div className="header__content">
+        <h3>{heading3}</h3>
+        <h1>{heading1}</h1>
+        <h2>{heading2}</h2>
+        {children}
+      </div>
+
+      <div className="header__image header__image--background-color">
+        <img
+          src={state.image.monster3}
+          alt="monster blue and pink"
+          className="header__image--monster3"
+        ></img>
+      </div>
+    </>
   );
 }
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
-import React from "react";
+import {useState} from "react";
+
 
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
 function Home() {
+  // 
+  const [state] = useState({
+    heading3:"Web Developoment And Style",
+    heading2:"Front-end developer",
+    heading1:"Alvin Gomez"
+  })
   return (
     <div className="home">
       <Sidebar />
-      <Header />
+      <Header heading3={state.heading3} heading2={state.heading2} heading1={state.heading1}>
+      <button className="btn">Check me out </button>  
+      </Header>
     </div>
   );
 }
